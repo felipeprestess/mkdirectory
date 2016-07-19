b66d8d489d7675dd071c5b3ab0b3db147e02e030
felipeprestess <felipeadriano_pc@hotmail.com>
2016-07-19 14:14:06 -0300

    Adicionado novos comandos ao MKVirtual em WPF

---
+++ MKVirtualWPF/MKVirtualWPF/Cliente.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CadastroCliente
+{
+    public class Cliente
+    {
+        string nome;
+        public string Nome 
+        {  
+            get { return nome; }
+            set{ nome = value; }
+        }
+        int idade;
+        public int Idade 
+        { 
+            get{return idade;}
+            set { idade = value; }
+        }
+        string email;
+        public string Email
+        { 
+            get { return email; }
+            set { email = value; }
+        }
+        int telefone;
+        public int Telefone 
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+        string sexo;
+        public string Sexo 
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+        private List<Cliente> listaClientes = new List<Cliente>();
+
+      
+        
+
+
+        /*Método de adicionar cliente*/
+        public void adicionaCliente(string nome, int idade, string email, int fone, string sexo)
+        {
+            Cliente person = new Cliente() { Nome = nome, Idade = idade, Email = email, Telefone = fone, Sexo = sexo};
+            
+            listaClientes.Add(person);
+
+            MessageBox.Show(string.Format("Cliente adicionado com sucesso!" , "Cadastro Cliente"));
+
+            
+                
+        }
+
+      
+
+        public void pesquisaCliente(Cliente p)
+        {
+            Cliente pes = new Cliente() { Nome = p.Nome };
+             foreach (Cliente cliente in listaClientes)
+                {
+                    if (pes.Nome == cliente.Nome)
+                    {
+                        MessageBox.Show(string.Format("Nome: {0}\nEmail: {1}\nTelefone: {2}", cliente.nome, cliente.email,cliente.telefone));
+                        break;
+                    }
+                   
+                    
+                   
+                }
+                 
+            
+        }
+
+
+        public void listarTodosClientes()
+        {
+            foreach (Cliente cliente in listaClientes)
+            {
+                MessageBox.Show(string.Format("Nome: {0}\n Idade: {1}\n Email: {2}\n Fone: {3}\n Sexo: {4}",cliente.nome, cliente.idade, cliente.email, cliente.telefone, cliente.sexo));
+            }
+        }
+
+
+
+        
+    }
+}
--- MKVirtualWPF/MKVirtualWPF/WindowCliente.xaml.cs
+++ MKVirtualWPF/MKVirtualWPF/WindowCliente.xaml.cs
@@ -9,25 +9,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MKVirtualWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WindowCliente1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WindowCliente : Window
     {
-        public MainWindow()
+        public WindowCliente()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            WindowCliente window = new WindowCliente();
-            window.Show();
         }
     }
 }
